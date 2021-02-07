973bd5a5671f4572792509da19d4ac9788f563db
Artem <64410376+Artiss90@users.noreply.github.com>
2021-02-07 22:53:09 +0200

    Cast

--- src/Component/Cast/Cast.jsx
+++ src/Component/Cast/Cast.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { fetchCast } from 'Services/API';
+import style from './Cast.module.css';
 
 class Cast extends Component {
   state = {
     cast: [],
   };
   async componentDidMount() {
-    const propId = this.props.match.params.movieId;
+    const propId = await this.props.match.params.movieId;
     const response = await fetchCast(propId);
     this.setState({ cast: response.data.cast });
   }
@@ -16,7 +17,21 @@ class Cast extends Component {
     return (
       <>
         <h1>Cast</h1>
-        {cast !== [] && cast.map(act => console.log(act.name, act.character))}
+        {/* // *с помощью popularity регулируем количество отображаемых актёров  */}
+        {cast !== [] && (
+          <ul>
+            {cast
+              .filter(actor => actor.popularity > 5)
+              .map(actor => (
+                <li key={actor.id} className={style.list}>
+                  <p className={style.name}>{actor.name}</p>
+                  <p className={style.role}>
+                    Role: {actor.character || 'minor roles'}
+                  </p>
+                </li>
+              ))}
+          </ul>
+        )}
       </>
     );
   }
